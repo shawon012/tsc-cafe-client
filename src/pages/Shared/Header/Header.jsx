7d5c8e85a548716c44042c721623d6566764ab10
shawon012 <shawon.shakib01@gmail.com>
2023-05-03 01:16:54 +0600

    header done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Navbar bg="light" variant="light">
+            <Container className='d-flex items-center'>
+                <Nav className='d-flex'>
+
+                    <img src="/tlogo.png" alt="" height="70px" />
+
+                    <h3 className='mt-3'>The TSC Cafe</h3>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Blog</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Button variant="secondary">Login</Button>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
