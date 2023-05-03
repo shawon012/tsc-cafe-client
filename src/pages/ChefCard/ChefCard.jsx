ff101d98657d1306925bb9a1b081aa2beb17382d
shawon012 <shawon.shakib01@gmail.com>
2023-05-03 06:17:30 +0600

    home section done

--- src/pages/ChefCard/ChefCard.jsx
+++ src/pages/ChefCard/ChefCard.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const ChefCard = ({ chef }) => {
     const { chef_picture, chef_name, chef_id, year_of_experience, number_of_recipes, likes, short_bio } = chef
     return (
-        <div>
-            <div className=' border border-primary rounded mb-4 d-flex align-items-center pe-4'>
-                <div className='flex-grow-1 d-flex gap-3'>
-                    <img src={chef_picture} alt="" height='350px' width='300px' className='rounded' />
+                <Col className='col-4 gap-3  rounded mb-5'>
                     <div className=''>
-                        <h3 className='pt-5'>{chef_name}</h3>
-                        <h4>Year of Experience: {year_of_experience}</h4>
-                        <h5>Number of Recipies: {number_of_recipes}</h5>
-                        <h5>Likes: {likes}</h5>
+                        <img src={chef_picture} alt="" height='350px' width='100%' className='rounded' />
+                        <div className=''>
+                            <h3 className='pt-2'>{chef_name}</h3>
+                            <h4>Year of Experience: {year_of_experience}</h4>
+                            <h5>Number of Recipies: {number_of_recipes}</h5>
+                            <h5>Likes: {likes}</h5>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <Button variant="outline-success">View Recipe</Button>
-                </div>
-            </div>
-        </div>
+                    <div>
+                        <Button variant="outline-success">View Recipe</Button>
+                    </div>
+                </Col>
+           
     );
 };
 
-export default ChefCard; 
+export default ChefCard;
 
 
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -23,14 +23,58 @@ const Home = () => {
                     <p>Curious? Here are our most popular chefs</p>
                 </div>
                 <div>
-                   {
-                    chefs.map(chef => <ChefCard
-                    key={chef.chef_id}
-                    chef={chef}
-                    ></ChefCard>)
-                   }
+                    <Container>
+                        <Row>
+                            {
+                                chefs.map(chef => <ChefCard
+                                    key={chef.chef_id}
+                                    chef={chef}
+                                ></ChefCard>)
+                            }
+                        </Row>
+                    </Container>
                 </div>
             </div>
+            <div>
+                <div className='text-center mt-5'>
+                    <h3>What Our Clients Say!</h3>
+                    <p>Learn Review from Our Previous Clients And Be Confident</p>
+                </div>
+                <div>
+                    <Container>
+                        <Row>
+                            <Col className='col-4 gap-3  rounded mb-5'>
+                                <div className=''>
+                                    <img src={`https://images.unsplash.com/photo-1682997843688-94722786a722?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`} alt="" height='350px' width='100%' className='rounded' />
+                                    <div className=''>
+                                        <h4>Very good service all the time. I am regularly taking service from here. This is one of the bes cafe.</h4>
+                                        <h3 className='pt-2'>Honey Lonne</h3>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col className='col-4 gap-3  rounded mb-5'>
+                                <div className=''>
+                                    <img src={`https://images.unsplash.com/photo-1682862261357-31c4ec647807?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`} alt="" height='350px' width='100%' className='rounded' />
+                                    <div className=''>
+                                        <h4>However I like the test of the food from this cafe very much. Specially burger is liked by me very much.</h4>
+                                        <h3 className='pt-2'>Julia Cirea</h3>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col className='col-4 gap-3  rounded mb-5'>
+                                <div className=''>
+                                    <img src={`https://images.unsplash.com/photo-1682997843692-c3cf1cb9caf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`} alt="" height='350px' width='100%' className='rounded' />
+                                    <div className=''>
+                                        <h4>I always like food. But this cafe is just doing good. I like the cheese cake most. But other recipies are also good.</h4>
+                                        <h3 className='pt-2'>Ciara Jara</h3>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+
         </Container>
     );
 };
